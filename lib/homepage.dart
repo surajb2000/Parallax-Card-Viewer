9642f36282049a65feb60b52ee86ec217d08636f
Suraj Bansal <36984962+surajb2000@users.noreply.github.com>
2021-06-12 00:05:48 +0530

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:parallax_effect/bottomSheet.dart';
+import 'package:parallax_effect/slider.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(
+          'Parallax Effect',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                height: 80,
+                width: double.infinity,
+              ),
+              SlidingCardsView(),
+            ],
+          ),
+          // ExhibitionBottomSheet(),
+          // Todo : Make extendable bottom sheet
+        ],
+      ),
+    );
+  }
+}
